e2ed61e5f43b2b9bb1bc1ab012f3fef82756cca4
BigOrangeApple <93687782+BigOrangeApple@users.noreply.github.com>
2021-11-14 13:56:11 -0500

    temp diff

--- robot.py
+++ robot.py
@@ -13,7 +13,7 @@ SHARPNESS = 3
 SHARPNESS_COLOR = 0.7 
 SPEED = 100
 WHEEL_CIRCUMFERENCE = 180
-TURN_SPEED = 45
+TURN_SPEED = 30
 
 class Robot():
     def __init__(self):
@@ -22,10 +22,12 @@ class Robot():
         self.right_wheel = Motor(Port.C)
         self.left_cs = ColorSensor(Port.S2)
         self.right_cs = ColorSensor(Port.S3)
+        self.ultra = UltrasonicSensor(Port.S4)
         self.tank = DriveBase(self.left_wheel, self.right_wheel, 50, 110)
-        self.gyro = GyroSensor(Port.S1) 
-        self.forklift = Motor(Port.D)
+        self.gyro = GyroSensor(Port.S1)
+        self.lift = Motor(Port.A)
         self.cs_threshold = self.read_calibrate()
+
 
     def follow_line(self, millies, speed=100):
         ''' Follows the line for a certain distance
@@ -40,6 +42,20 @@ class Robot():
             self.tank.drive(speed, multiply) 
         self.brake()
 
+    def follow_line_to_black(self, speed=100):
+        '''  Follows the line untill both sensors are on black
+        '''
+        while not self.stop_on_black(): 
+            subtract = self.left_cs.reflection() - self.right_cs.reflection() 
+            multiply = subtract * SHARPNESS_COLOR
+            self.tank.drive(speed, multiply) 
+        self.brake()
+
+    def read_calibrate(self):
+        file_handler = open("Calibration.txt", "r")
+        int_value = int(file_handler.read())
+        return int_value
+
     def brake(self):
         ''' brakes the robot
         '''
@@ -50,7 +66,7 @@ class Robot():
         self.right_wheel.brake()
         self.left_wheel.brake()
 
-    def gyro_straight(self, target):
+    def gyro_straight(self, target, speed=SPEED):
         ''' Goes forward until it reaches black trying to keep gyro angle straight.
     
         Args:
@@ -59,7 +75,7 @@ class Robot():
         while not self.stop_on_black(): 
             subtract = self.gyro.angle() - target  
             multiply = subtract * (SHARPNESS * -1) 
-            self.tank.drive(SPEED, multiply)
+            self.tank.drive(speed, multiply)
         self.brake() 
     
     
@@ -89,20 +105,22 @@ class Robot():
         self.forklift.run_angle(speed, degrees)
         self.brake()
     
-    
     def gyro_angle(self, angle):
         self.brake()
         big = angle + 1
         small = angle - 1
         print('gyro_angle begin: ' + str(self.gyro.angle()))
         while self.gyro.angle() <= small or self.gyro.angle() >= big:
-            while self.gyro.angle() <= small or self.gyro.angle() >= big: 
+            while self.gyro.angle() <= small or self.gyro.angle() >= big:
+                speed = TURN_SPEED
+                if abs(self.gyro.angle() - angle) > 5:
+                    speed = 100 
                 if self.gyro.angle() <= small:
-                    self.left_wheel.run(TURN_SPEED)
-                    self.right_wheel.run(TURN_SPEED * -1)
+                    self.left_wheel.run(speed)
+                    self.right_wheel.run(speed * -1)
                 else:
-                    self.left_wheel.run(TURN_SPEED * -1)
-                    self.right_wheel.run(TURN_SPEED)
+                    self.left_wheel.run(speed * -1)
+                    self.right_wheel.run(speed)
             self.brake()
             time.sleep(0.4)    
         self.brake()
@@ -112,10 +130,10 @@ class Robot():
     def stop_on_black(self, ignore_left=False, ignore_right=False):
         ''' Used so the robot can identify black
         '''
-        if (self.left_cs.reflection() <= 9 or ignore_left) and (
-            self.right_cs.reflection() <= 9 or ignore_right):
-            return True
+        if (self.left_cs.reflection() <= 12 or ignore_left) and (
+            self.right_cs.reflection() <= 12 or ignore_right):
             self.brake()
+            return True
         else:
             return False
     
@@ -142,56 +160,48 @@ class Robot():
 
     def steering(self, speed, sharpness, distance):
         ''' Makes the robot able to turn and go forward
-
         Args:
-
         '''
         self.tank.reset()
         big = distance
         small = -1 * distance
         while self.tank.distance() >= small and self.tank.distance() <= big:
             self.tank.drive(speed, sharpness)
-        self.brake()      
+        self.brake()   
     
-    def read_calibrate(self):
-        file_handler = open("Calibration.txt", "r")
-        int_value = int(file_handler.read())
-        return int_value
-
     def steering_angle(self, speed, sharpness, angle):
-    ''' Steers the robot forward with `speed` and `sharpness` until a certain angle
+        ''' Steers the robot forward with `speed` and `sharpness` until a certain angle
 
-    Preconditions:
-        sharpness: should always be positive 
+        Preconditions:
+            sharpness: should always be positive 
 
-    Args:
-        speed: the speed of the steering
-        sharpness: the sharpness or urgency
-        angle: the angle the robot is aiming to stop at
-    '''
-    print("Beginning of steering_angle " + str(self.gyro.angle()))
-    big = angle + 1
-    small = angle - 1
-    temp_sharp = sharpness
-    if self.gyro.angle() > angle:  # Should turn left
-        temp_sharp = sharpness * -1
-        while self.gyro.angle() <= small or self.gyro.angle() >= big:
-            if self.gyro.angle() >= big:
-                self.tank.drive(speed, temp_sharp)
-            elif self.gyro.angle() <= small:
-                self.tank.drive(speed * -1, temp_sharp * -1)
-    elif self.gyro.angle() < angle:  # Should turn right
-        while self.gyro.angle() <= small or self.gyro.angle() >= big:
-            if self.gyro.angle() <= small:
-                self.tank.drive(speed, temp_sharp)
-            elif self.gyro.angle() >= big:
-                self.tank.drive(speed * -1, temp_sharp * -1)
-    self.brake()
-    print("End of steering_angle " + str(self.gyro.angle()))
+        Args:
+            speed: the speed of the steering
+            sharpness: the sharpness or urgency
+            angle: the angle the robot is aiming to stop at
+        '''
+        print("Beginning of steering_angle " + str(self.gyro.angle()))
+        big = angle + 1
+        small = angle - 1
+        temp_sharp = sharpness
+        if self.gyro.angle() > angle:  # Should turn left
+            temp_sharp = sharpness * -1
+            while self.gyro.angle() <= small or self.gyro.angle() >= big:
+                if self.gyro.angle() >= big:
+                    self.tank.drive(speed, temp_sharp)
+                elif self.gyro.angle() <= small:
+                    self.tank.drive(speed * -1, temp_sharp * -1)
+        elif self.gyro.angle() < angle:  # Should turn right
+            while self.gyro.angle() <= small or self.gyro.angle() >= big:
+                if self.gyro.angle() <= small:
+                    self.tank.drive(speed, temp_sharp)
+                elif self.gyro.angle() >= big:
+                    self.tank.drive(speed * -1, temp_sharp * -1)
+        self.brake()
+        print("End of steering_angle " + str(self.gyro.angle()))
 
     def straight_distance(self, distance, speed=150):
         ''' Goes straight for a certain distance
-
         Args:
             speed: the speed of the robot
             distance: the distance the robot should go
@@ -211,3 +221,5 @@ class Robot():
         while not self.stop_on_black(ignore_left, ignore_right):
             self.tank.drive(speed, 0)
         self.brake()
+                
+    
